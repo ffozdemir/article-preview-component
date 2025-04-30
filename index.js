cbcfd7e9b7f0a465478f66661d977cb4d59c9f70
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2025-04-30 23:53:26 +0200

    Enhance click event handling for social icons and add CSS for positioning

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const authorInfoEl = document.querySelector(".author-info");
 const socialIconsEl = document.querySelector(".social-icons");
 const cardAuthorEl = document.querySelector(".card-author");
 
-iconShareEl.addEventListener("click",  () => {
-   authorInfoEl.classList.toggle("none");
-   socialIconsEl.classList.toggle("none");
-   cardAuthorEl.classList.toggle("very-dark-grayish-blue");
-   iconShareEl.classList.toggle("light-grayish-blue");
-   iconShareEl.classList.toggle("grayish-blue");
+iconShareEl.addEventListener("click", () => {
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    cardAuthorEl.classList.toggle("very-dark-grayish-blue");
+    authorInfoEl.classList.toggle("none");
+  } else {
+    socialIconsEl.classList.toggle("social-icons-position");
+  }
+  socialIconsEl.classList.toggle("none");
+  iconShareEl.classList.toggle("light-grayish-blue");
+  iconShareEl.classList.toggle("grayish-blue");
 });
